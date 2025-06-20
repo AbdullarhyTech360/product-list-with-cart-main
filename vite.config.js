c4bca82276d7409f70f8e09181bb7e70ff3888bb
AbdullarhyTech360 <abdullahimaikidandan@gmail.com>
2025-06-20 10:26:32 +0100

    Update vite config and favicon path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/', // Replace with your actual repo name
+  base: '/product-list-with-cart-main/',
   build: {
     outDir: 'dist'
   }
